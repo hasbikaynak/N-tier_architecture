435f2b715345f7370f41d458b5fa0bdcd901654f
Mehmet Hasbi KAYNAK <73940626+BrOlaf45@users.noreply.github.com>
2021-02-11 14:58:39 +0100

    create Costumer.cs , Create DataAccessLayer.cs

---
+++ N-tier_architecture/Costumer.cs
@@ -0,0 +1,9 @@
+﻿namespace N_tier_architecture
+{
+    internal class Costumer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
---
+++ N-tier_architecture/DataAccessLayer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N_tier_architecture
+{
+     internal class DataAccessLayer
+    {
+        SqlConnection connection;
+        SqlCommand command;
+        SqlDataReader reader;
+        public DataAccessLayer()
+        {
+            connection = new SqlConnection();
+            connection.ConnectionString = "Data Source=.; Initial Catalog = Adonet; User ID=sa; Password =1";
+        }
+        internal void ConnectionOptions()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            
+                connection.Open();
+            
+            else
+            
+                connection.Close();
+        }
+
+            internal int SaveData(Costumer C)
+            {
+            //It is going to be saving to database proccess here
+            return 1;
+            }
+        
+    }
+
+    
+}
