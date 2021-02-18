e27a9fc98d69216b1009a03d678ceeb45e9ac9bf
Mehmet Hasbi KAYNAK <73940626+BrOlaf45@users.noreply.github.com>
2021-02-18 07:47:22 +0100

    Create BussinessLogicLayer class

---
+++ N-tier_architecture/BusinessLogicLayer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N_tier_architecture
+{
+   internal class BusinessLogicLayer
+    {
+        DataAccessLayer dal;
+        public BusinessLogicLayer()
+        {
+            new DataAccessLayer();
+        }
+
+        internal int SaveData(int CostumerID, string Name, string Surname)
+        {
+            if (!string.IsNullOrEmpty(Name)&&!string.IsNullOrEmpty(Surname))
+            {
+                Costumer C1 = new Costumer();
+                C1.ID = CostumerID;
+                C1.Name = Name;
+                C1.Surname = Surname;
+               return  dal.SaveData(C1); // we cleaned up our codes and data now we can carry it up to the next tier 
+            }
+            else
+            {
+                return -1; // This is going to provide an missing parameter error.
+            }
+        }
+    }
+}
